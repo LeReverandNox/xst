94a1891439c2c31e6ba1876e30a2a346ffd322e4
LeReverandNox <lereverandnox@gmail.com>
2024-02-28 18:06:52 +0100

    Update config.h from config.def.h

--- config.h
+++ config.h
@@ -6,6 +6,10 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font2[] = {
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+};
 
 /* disable bold, italic and roman fonts globally */
 int disablebold = 0;
@@ -32,7 +36,7 @@ static unsigned int borderless = 0;
 /* allow infelicity between the weights: */
 unsigned int max_bold_weight_infelicity = 20;
 
-static unsigned int borderpx = 2;
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -167,8 +171,10 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-	"black",
-	"gray90",
+	"gray90", /* default foreground colour */
+	"black", /* default background colour */
+	"gray90", /* foreground colour (reverse) */
+	"black", /* background colour (reverse) */
 };
 
 
@@ -176,9 +182,11 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 259;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
+unsigned int defaultfg_reverse = 260;
+unsigned int defaultbg_reverse = 261;
+unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
 /*
@@ -233,6 +241,10 @@ static MouseShortcut mshortcuts[] = {
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
 
+/* Command for opening url in browser */
+static char *openurlcmd[] = {"/bin/sh", "-c", "xurls | eval dmenu $(dmenu_options) | xargs -r $BROWSER",
+                             "externalpipe", NULL};
+
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
@@ -253,7 +265,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ MODKEY,               'u',            externalpipe,   {.v = "xurls | eval dmenu $(dmenu_options) | xargs -r $BROWSER" } },
+	{ MODKEY,               'u',            externalpipe,   {.v = openurlcmd } },
 };
 
 /*
@@ -525,3 +537,9 @@ static char ascii_printable[] =
 	" !\"#$%&'()*+,-./0123456789:;<=>?"
 	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
 	"`abcdefghijklmnopqrstuvwxyz{|}~";
+
+/*
+ * render method of undercurls
+ */
+static unsigned int undercurl_shape = 1;
+static unsigned int undercurl_thickness_threshold = 28;
